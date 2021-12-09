1fe0be3276e4120dae11a067afcb566013a27c0d
Crescent17 <maratos2001@mail.ru>
2021-12-09 10:48:32 +0300

    commit

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+import weka.core.Instance;
+import weka.core.Instances;
+import weka.core.converters.ConverterUtils.DataSource;
+import weka.classifiers.functions.LinearRegression;
+
+public class Main {
+    public static void main(String args[]) throws Exception {
+        DataSource source = new DataSource("D:\\Prog\\Java\\WEKA\\resources\\house.arff");
+        Instances dataset = source.getDataSet();
+        dataset.setClassIndex(dataset.numAttributes() - 1);
+        LinearRegression model = new LinearRegression();
+        model.buildClassifier(dataset);
+        System.out.println("LR FORMULA : " + model);
+        Instance myHouse = dataset.lastInstance();
+        double price = model.classifyInstance(myHouse);
+        System.out.println("-------------------------");
+        System.out.println("PRECTING THE PRICE : " + price);
+    }
+}
